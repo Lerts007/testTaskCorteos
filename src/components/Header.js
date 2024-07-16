d0b790fd95e30f9f0496d4a8b01ed6997ddf2f07
Lerts <oddff@mail.ru>
2024-07-17 00:05:51 +0300

    поменял местами города

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ export default {
         <h1 class="logo__title">Туда</h1>
       </div>
       <div class="town-date">
-        <p class="town-date__town">Москва <span class="icon-arrow-right town-date__town__arrow"></span> Сантк-Петербург</p>
+        <p class="town-date__town">Сантк-Петербург <span class="icon-arrow-right town-date__town__arrow"></span> Москва</p>
         <p class="town-date__date"><span class="icon-calendar town-date__calendar"></span>19 февраля, пт.</p>
       </div>
     </div>
